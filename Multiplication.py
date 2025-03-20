8f39054810fd79d27766edb0d29a11fe2d07635b
Gurnoor Kaushal <149480822+Gurnoor123kaushal@users.noreply.github.com>
2025-03-20 13:05:55 +0530

    Create Multiplication.py

---
+++ Multiplication.py
@@ -0,0 +1,9 @@
+# Get user input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Multiply the numbers
+result = num1 * num2
+
+# Display the result
+print(f"The result of multiplying {num1} and {num2} is: {result}")
